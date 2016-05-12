7b63b598b1204cfad2d917b6ecb7d3b353d8842c
Jinman Zhao <jinman.zhao@gmail.com>
2016-05-12 17:29:38 -0500

    in dev: design test cases

--- pdr/test.py
+++ pdr/test.py
@@ -12,8 +12,8 @@ import logging
 
 logging.basicConfig(level=logging.INFO)
 
-x, y, z = Bools("x y z")
-xp, yp, zp = Bools("x' y' z'")
+a, b, c, d, e, f, x, y, z = Bools("a b c d e f x y z")
+ap, bp, cp, dp, ep, fp, xp, yp, zp = Bools("a' b' c' d' e' f' x' y' z'")
   
 test_cases = list()
 
@@ -43,13 +43,107 @@ case.update({
 })
 test_cases.append(case) 
 
+
+case = {
+    'name' : "easy-adder-safe",
+    'description' : """An safe case for an adder that increases two to the
+  binary representation $\hat{abcd}$ upon each step. The equivalent program is
+  <pseudocode>
+  assert abcd == 0
+  forever {
+    abcd += 2  # overflow bit is ignored
+  }
+  assert mod(abcd, 2) == 0
+  </pseudocode>""",
+    'bool_pairs' : [(a, ap), (b, bp), (c, cp), (d, dp)],
+    'init' : And(Not(a), Not(b), Not(c), Not(d)),
+    'post' : Not(d),
+    'trans' : And(dp == d, cp == Xor(c, True), bp == Xor(b, c), ap == Xor(a, And(b, c))),
+    'expected_result' : {
+        'check_res' : SAFE,
+        'inv' : Not(d),
+    },
+}
+test_cases.append(case)  
+
+case = {
+    'name' : "easy-adder-unsafe",
+    'description' : """An unsafe case for an adder that increases two to the
+  binary representation $\hat{abcd}$ upon each step. The equivalent program is
+  <pseudocode>
+  assert abcd == 1  # the place that makes the program unsafe
+  forever {
+    abcd += 2  # overflow bit is ignored
+  }
+  assert mod(abcd, 2) == 0
+  </pseudocode>""",
+    'bool_pairs' : test_cases[-1]['bool_pairs'],
+    'init' : And(Not(a), Not(b), Not(c), (d)),
+    'post' : Not(d),
+    'trans' : test_cases[-1]['trans'],
+    'expected_result' : {
+        'check_res' : UNSAFE,
+        'ce_start' : {d: True},
+    },
+}
+test_cases.append(case)  
+
+case = {
+    'name' : "marginal-algebra-safe",
+    'description' : """An safe case for some combination of algebra operations.
+  The equivalent program is
+  <pseudocode>
+  assert abcdef < 16
+  forever {
+    abcdef += 24
+    abcdef >>= 1
+  }
+  assert abcdef <= 24
+  </pseudocode>""",
+    'bool_pairs' : [(a, ap), (b, bp), (c, cp), (d, dp), (e, ep), (f, fp)],
+    'init' : And(Not(a), Not(b)),
+    'post' : And(Not(a), Implies(And(b, c), Not(Or(d, e, f)))),
+    'trans' : And(fp == e, ep == d, dp == Xor(c, True), cp == Xor(b, c),
+                  bp == Xor(a, And(b, c)), ap == False),
+    'expected_result' : {
+        'check_res' : SAFE,
+        'inv' : "sorry, I don't know. Maybe the same as the initial. Or abcdef <= 23 ", 
+    },
+}
+test_cases.append(case)  
+
+case = {
+    'name' : "marginal-algebra-unsafe",
+    'description' : """An unsafe case for some combination of algebra operations.
+  The equivalent program is
+  <pseudocode>
+  assert abcdef < 16
+  forever {
+    abcdef += 28 # the place that makes the program unsafe
+    abcdef >>= 1
+  }
+  assert abcdef <= 24
+  </pseudocode>""",
+    'bool_pairs' : test_cases[-1]['bool_pairs'],
+    'init' : test_cases[-1]['init'],
+    'post' : And(Not(a), Implies(And(b, c), Not(Or(d, e, f)))),
+    'trans' : And(fp == e, ep == Xor(d, True), dp == Xor(c, d), 
+                  cp == Xor(b, And(c, d)),
+                  bp == Xor(a, And(b, c, d)), ap == False),
+    'expected_result' : {
+        'check_res' : UNSAFE,
+        'ce_seq' : {}, # any state within the initial state
+    },
+}
+test_cases.append(case)  
+
 safty_names = {
     SAFE : "SAFE",
     UNSAFE : "UNSAFE",
     UNKNOWN : "UNKNOWN",
 }
 
-for i_case, case in enumerate(test_cases) :
+for i_case, case in enumerate(test_cases[-2:]) :
     print("checking for test case %d..."%i_case)
     print("case name: %s"%case['name'])
     print("case description:", case['description'])
@@ -87,7 +181,7 @@ for i_case, case in enumerate(test_cases) :
         print("checking counterexample sequence...", end=" ")
         got = ce_seq
         exp = (case['expected_result'].get('ce_start'))
-        if is_tautology(state_to_cube(ce_seq[0]) == state_to_cube(exp)) :
+        if is_tautology(Implies(state_to_cube(ce_seq[0]), state_to_cube(exp))) :
             print("Correct!")
         else :
             print("Unexpected.")
